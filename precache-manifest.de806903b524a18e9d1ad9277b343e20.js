4268a1d80a6cf45b4a01643ee6c8973feeac5cda
Manoj Kumar E <manoj.e@nectechnologies.in>
2020-05-30 00:26:15 +0530

    Updates

---
+++ precache-manifest.de806903b524a18e9d1ad9277b343e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fab8601ff53494ec4a2d72c0a5830e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "64dc59f3b408d930eebb",
+    "url": "/monsters-rolodex/static/css/main.ef146e30.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64dc59f3b408d930eebb",
+    "url": "/monsters-rolodex/static/js/main.6745bb45.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
